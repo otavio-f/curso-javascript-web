51832690db64e68166b5b5083551e294722f109c
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-11-27 08:35:14 -0300

    feat: Aula 7

---
+++ Seção 2 - Fundamentos/6. Hello World.js
@@ -0,0 +1,10 @@
+alert("Hello World");
+
+let js = "amazing";
+
+if (js === "amazing") alert("Javascript is FUN!");
+//mostra janela
+
+ls = "boring";
+
+if (js === "amazing") alert("Javascript is FUN!");
