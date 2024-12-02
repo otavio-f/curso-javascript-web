4b72c5e6fd138bbd8a1274efe892ba9bc0c6b346
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-12-02 10:23:52 -0300

    feat: Aula 73

--- Seção 7 - Javascript no Browser/guess-my-number/script.js
+++ Seção 7 - Javascript no Browser/guess-my-number/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message'));
 
 document.querySelector('.message').textContent = 'HEH';
@@ -11,3 +12,14 @@ document.querySelector('.score').textContent = '9999';
 document.querySelector('.guess').value = 99; // set the value this way
 
 console.log(document.querySelector('.guess').value); // get the value this way
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    console.log('No number.');
+    return;
+  }
+
+  console.log(guess);
+});
