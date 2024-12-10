d8683bb57a3fe8f6bd69362d99c59e9c42d45f6b
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-12-10 20:29:14 -0300

    feat: Aula 95

--- Seção 8 - Como Javascript funciona internamente/behind-the-scenes/script.js
+++ Seção 8 - Como Javascript funciona internamente/behind-the-scenes/script.js
@@ -44,3 +44,66 @@ calcAge(1991);
 
 // console.log(age); // Escopo global não tem acesso a variáveis de printAge(). Isso vai dar ReferenceError.
 // printAge(); // Escopo global não tem acesso a escopo interno de calcAge(). ReferenceError de novo.
+
+/*
+ * Hoisting
+ */
+
+// Hoisting de variáveis
+
+console.log(me); // Esse vai retornar undefined, pois é o valor atribuído com o hoisting
+// console.log(job);
+// console.log(year);
+// Esses vão dar erro pois não foram declarados ainda e hoisting não os "puxa" pra cima.
+
+var me = 'Otavio';
+let job = 'student';
+const year = 1990;
+
+// Hoisting de funções
+
+console.log(addFuncDecl(2, 3)); // Esse funciona, funções declaradas são "puxadas" pra o topo do bloco
+// console.log(addExpr(2, 3)); // Dá erro, a função é uma variável const
+// console.log(addArrow(2, 3)); // Dá erro, arrow functions ainda são variáveis. Chamar var é equivalente a chamar undefined
+
+function addFuncDecl(a, b) {
+  return a+b;
+}
+
+const addExpr = function(a, b) {
+  return a+b;
+}
+
+var addArrow = (a,b) => a+b;
+
+/*
+ * Porque var é perigoso
+ */
+
+if(!numProducts) // poderia comparar com zero, porém só checar por valores falsey parece mais fácil
+  deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() { // função muito perigosa que deve ser chamada com cuidado
+  console.log("Shopping Cart deleted!");
+}
+
+// Hoisting faz com que numProducts "suba" com valor undefined, que é um valor falso
+// A função é chamada assim mesmo
+// Resumindo: NÃO USE VAR!!!
+
+/* 
+ * var e objeto window
+ */
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // verdadeiro
+console.log(y === window.y); // falso
+console.log(z === window.z); // falso
+
+// Além disso variáveis var adicionam uma propriedade no objeto global window, enquanto que let e const não fazem isso
+
