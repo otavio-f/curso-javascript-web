4ce364be1a83b89e3186584932255d0e264aa0ed
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-12-22 11:32:10 -0300

    feat: Aula 133

--- Seção 10 - Funções/functions/script.js
+++ Seção 10 - Funções/functions/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+/**
+ * 133. Parâmetros padrão (ES6)
+ */
+const bookings = [];
+
+//// É possível usar expressões no argumentos padrão
+// function createBooking(flightNum, numPassengers = 1, price = 199 * numPassengers) {...}
+// Atenção: As expressões consideram a ordem dos argumentos. Uma expressão só pode usar um argumento definido antes
+
+//// Maneira antiga (ES5) de setar argumentos padrão (dentro da função)
+//// Argumentos ficam sem atribuição
+// numPassengers = numpassengers || 1;
+// price = price || 199;
+
+function createBooking(flightNum, numPassengers = 1, price = 199) {
+  /*
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  */
+  // Eu acho a sintaxe curta mais propensa a bugs, por isso não gosto de usá-la
+  const booking = {
+    flightNum: flightNum,
+    numPassengers: numPassengers,
+    price: price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking('LH123');
+createBooking('LH123', 2);
+createBooking('LH123', 3, 150);
+// Atenção: Não é possível pular parâmetros!!
+// Para usar um parâmetro padrão, defina-o como undefined
+createBooking('LH234', undefined, 250);
