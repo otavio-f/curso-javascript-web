3a527c599fae09ccb3b182f1fcefb5f8737c5424
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2025-01-05 21:30:54 -0300

    feat: Aula 196

--- Seção 13 - DOM avançado e eventos/bankist/script.js
+++ Seção 13 - DOM avançado e eventos/bankist/script.js
@@ -29,3 +29,57 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/**
+ * 196. Selecionando, criando e deletando elementos
+ */
+
+//// Selecionando elementos ////
+
+// use propriedades do objeto document pra selecionar elementos mais gerais da página
+
+console.log('Document:', document.documentElement); // O documento inteiro
+
+console.log('Head:', document.head); // o cabeçalho <head>
+
+console.log('Body:', document.body); // o corpo <body>
+
+// pra selecionar elementos mais específicos, é possível usar outros métodos
+
+console.log('class="header":', document.querySelector('.header')); // O primeiro elemento com class="header"
+console.log('All class="section":', document.querySelectorAll('.section')); // Todos os elementos com class="section"
+// Atenção: O argumento deve seguir o mesmo padrão de seleção de elementos em CSS!!
+// Atenção: querySelectorAll retorna uma NodeList, que --não-- acompanha as alterações feitas na página
+
+console.log('class="section--1":', document.getElementById('section--1')); // Só retorna um elemento
+console.log('All <button>:', document.getElementsByTagName('button')); // Todos os <button> da página
+console.log('All class="btn":', document.getElementsByClassName('btn')); // Todos os elementos com class="btn"
+// Atenção: getElements... retorna HTMLCollection, que acompanha as alterações feitas na página
+
+//// Criando e adicionando elementos ////
+
+// .insertAdjacentHTML // adiciona um elemento em uma posição relativa a outro
+const message = document.createElement('div'); // cria um <div> (ainda não está na página!)
+message.classList.add('cookie-message');
+// message.textContent =
+// 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it</button>';
+const header = document.querySelector('.header');
+
+header.prepend(message); // adiciona como primeiro elemento filho
+// header.apppend(message); // adiciona como último elemento filho
+// Atenção: Somente um pode ser usado com o mesmo elemento. Usando mais que um vai mover o elemento, não copiar
+
+// header.append(message.cloneNode(true)); // use .cloneNode(true) pra copiar o elemento
+// header.before(message); // adiciona antes do elemento
+// header.after(message); // adiciona depois do elemento
+
+//// Deletar elementos ////
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    // message.remove(); // deleta o elemento, maneira nova
+    message.parentElement.removeChild(message); // maneira mais antiga, só se pode remover um elemento filho
+  });
