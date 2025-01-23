9145fa74bf96e4d4b6292aac2ffc16ead7778be7
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2025-01-23 09:16:58 -0300

    feat: Aula 288

--- Seção 17 - Desenvolvimento moderno/starter/script.js
+++ Seção 17 - Desenvolvimento moderno/starter/script.js
@@ -2,7 +2,10 @@
 // Importação de módulo
 
 // import './shoppingCart.js'; // também funciona sem a extensão de arquivo
-import { addToCart } from './shoppingCart.js'; // pra importar named exports use o mesmo
+import { addToCart as atc, cart as ct } from './shoppingCart.js'; // pra importar named exports use o mesmo
+
+atc('pasta', 9);
+console.log(ct);
 
 // pra importar __todos__ os exports de um módulo use *
 // import * from './shoppingCart';
@@ -76,8 +79,8 @@ const ShoppingCart2 = (function () {
    * @param {*} quantity Quantidade do produto
    */
   function addToCart(product, quantity) {
-    while (quantity--) cart.push(product);
     console.log(`${quantity} ${product} added to cart.`);
+    while (quantity--) cart.push(product);
   }
 
   /**
@@ -143,6 +146,7 @@ console.log(ShoppingCart2);
 // "npm init" inicializa um novo projeto na pasta atual
 
 // "npm install leaflet" ou "npm i leaflet" instala o pacote leaflet no projeto atual
+// para instalar uma versão específica use @, "npm i leaflet@1.9.4"
 // pacotes são instalados na pasta ./node_modules
 // o nome do pacote é adicionado ao campo "dependencies" no arquivo package.json
 
@@ -174,3 +178,41 @@ const stateDeepClone = cloneDeep(state);
 
 // modificações no original não causará mudanças nessa cópia e vice-versa
 console.log(stateDeepClone);
+
+/**
+ * 288. Bundle com Parcel e scripts NPM
+ */
+
+// Parcel porque é mais fácil, já vem pronto pra usar
+// Babel também é uma opção, mais é bem mais complicado de usar (Usado em React)
+
+// pra instalar use o comando
+// "npm i parcel --save-dev"
+
+// --save-dev configura como dependência dev, que significa que o módulo não vai ser incluído no bundle final, mas é necessário
+// adiciona dependências na chave "devDependencies" ao invés de "dependencies"
+
+// pra rodar parcel use npx
+// "npx parcel index.html"
+// parcel vai criar bundle na pasta "./dist" e abrir um servidor local, como live-server
+
+// hot module replacement
+// quando houver alguma modificação, o novo código será injetado na página sem recarregar a página toda
+// use pra não mudar o estado da página quando salvar
+if (module.hot) {
+  module.hot.accept();
+}
+
+// caminhos de biblioteca
+// todos os bundlers suportam incluir somente o nome da biblioteca, sem o caminho completo
+
+// Exemplo: cloneDeep da importação mais acima
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+// import cloneDeep from 'lodash-es';
+// parcel e babel vão achar o caminho completo somente pelo nome da biblioteca
+// também conseguem carregar bibliotecas commonjs ou es6
+
+// É possível automatizar o comando para rodar o parcel
+// basta adicionar comandos (sem o npx) na chave "scripts" no arquivo ./package.json
+// use "npm run <nome do comando>" para rodar o comando
